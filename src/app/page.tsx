44d2706904b0d8f064dabb2a94f77004105cb767
Hafis-cnm <hafis@cnmindia.in>
2025-06-21 12:33:18 +0530

    Navbar for mobile and desktop completed, prisma setup and schema setup completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle></ModeToggle>
-      <Button variant={"secondary"}>Click me</Button>
+      Home page content
     </div>
   );
 }
